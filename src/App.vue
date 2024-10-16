a32d6722426d8bfbff6e22827d7803c782ec285a
tphuc29092003 <tphuc290923@gmail.com>
2024-10-16 18:23:55 +0700

    Khoi tao du an

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  //Đây là cú pháp để xuất một đối tượng mặc định. 
+  //Trong trường hợp này, đối tượng rỗng {} 
+  //nghĩa là component này không có bất kỳ thuộc tính hay phương thức nào được định nghĩa.
+}
+</script>
+  <template>//Phần này định nghĩa giao diện của component. Nó sử dụng HTML để mô tả cách mà giao diện sẽ hiển thị.
+    <h1>Hello, Vue.js!</h1>
+  </template>
+<style>
+  .page {
+    max-width: 400px;
+    margin: auto;
+  }
+</style>
